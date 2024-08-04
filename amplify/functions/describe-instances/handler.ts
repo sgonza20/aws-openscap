2704de49b6cda48e4c9a68b0fa71933092941f0d
Sam Lozano <sgonza20@vols.utk.edu>
2024-08-04 12:55:28 -0400

    fix: return array

--- amplify/functions/describe-instances/handler.ts
+++ amplify/functions/describe-instances/handler.ts
@@ -41,7 +41,7 @@ export const handler = async (event: any) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(instances),
+      body: instances,
     };
   } catch (error) {
     console.error("Error handling request:", error);
